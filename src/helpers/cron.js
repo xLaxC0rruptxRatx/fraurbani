0c3938ae2c1055eb01b9e7e5bd505759fb54884d
elpepe <herejeb0t@atomicmail.io>
2026-01-18 21:05:42 -0600

    updt1

--- src/helpers/cron.js
+++ src/helpers/cron.js
@@ -1,7 +1,7 @@
 import cron from 'cron'
 import https from 'https'
 
-const url = 'https://fraudbani-za2r.onrender.com'
+const url = 'https://fraudbani-fyfr.onrender.com'
 const job = new cron.CronJob('*/14 * * * *', function() {
   console.log('Reiniciando...')
   https
